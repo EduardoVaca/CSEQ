e89a2a15884709c5209213b12ef3a6d9cf3a157a
unknown <max.ram.car@gmail.com>
2015-03-21 20:22:53 -0600

    tablas de max

---
+++ CSEQ/CSEQ/Crear_delegacion.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace CSEQ
+{
+    partial class Crear_delegacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Crear_delegacion));
+            this.nombre_txt = new System.Windows.Forms.TextBox();
+            this.delegacion_label = new System.Windows.Forms.Label();
+            this.close_picture = new System.Windows.Forms.PictureBox();
+            this.atras_picture = new System.Windows.Forms.PictureBox();
+            this.municipio_combo = new System.Windows.Forms.ComboBox();
+            this.nombre_label = new System.Windows.Forms.Label();
+            this.municipio_label = new System.Windows.Forms.Label();
+            this.rompiendoParadigmas_label = new System.Windows.Forms.Label();
+            this.guardar_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.close_picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atras_picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nombre_txt
+            // 
+            this.nombre_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_txt.Location = new System.Drawing.Point(283, 226);
+            this.nombre_txt.MaximumSize = new System.Drawing.Size(400, 40);
+            this.nombre_txt.MinimumSize = new System.Drawing.Size(100, 4);
+            this.nombre_txt.Name = "nombre_txt";
+            this.nombre_txt.Size = new System.Drawing.Size(229, 38);
+            this.nombre_txt.TabIndex = 7;
+            // 
+            // delegacion_label
+            // 
+            this.delegacion_label.AutoSize = true;
+            this.delegacion_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delegacion_label.ForeColor = System.Drawing.Color.White;
+            this.delegacion_label.Location = new System.Drawing.Point(313, 161);
+            this.delegacion_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.delegacion_label.Name = "delegacion_label";
+            this.delegacion_label.Size = new System.Drawing.Size(176, 37);
+            this.delegacion_label.TabIndex = 6;
+            this.delegacion_label.Text = "Delegación";
+            // 
+            // close_picture
+            // 
+            this.close_picture.Image = ((System.Drawing.Image)(resources.GetObject("close_picture.Image")));
+            this.close_picture.Location = new System.Drawing.Point(759, 11);
+            this.close_picture.Margin = new System.Windows.Forms.Padding(2);
+            this.close_picture.Name = "close_picture";
+            this.close_picture.Size = new System.Drawing.Size(30, 29);
+            this.close_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.close_picture.TabIndex = 8;
+            this.close_picture.TabStop = false;
+            this.close_picture.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // atras_picture
+            // 
+            this.atras_picture.Image = ((System.Drawing.Image)(resources.GetObject("atras_picture.Image")));
+            this.atras_picture.Location = new System.Drawing.Point(12, 541);
+            this.atras_picture.Name = "atras_picture";
+            this.atras_picture.Size = new System.Drawing.Size(47, 47);
+            this.atras_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atras_picture.TabIndex = 9;
+            this.atras_picture.TabStop = false;
+            // 
+            // municipio_combo
+            // 
+            this.municipio_combo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.municipio_combo.FormattingEnabled = true;
+            this.municipio_combo.Location = new System.Drawing.Point(283, 313);
+            this.municipio_combo.Name = "municipio_combo";
+            this.municipio_combo.Size = new System.Drawing.Size(229, 39);
+            this.municipio_combo.TabIndex = 10;
+            // 
+            // nombre_label
+            // 
+            this.nombre_label.AutoSize = true;
+            this.nombre_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_label.ForeColor = System.Drawing.Color.White;
+            this.nombre_label.Location = new System.Drawing.Point(110, 226);
+            this.nombre_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.nombre_label.Name = "nombre_label";
+            this.nombre_label.Size = new System.Drawing.Size(132, 37);
+            this.nombre_label.TabIndex = 11;
+            this.nombre_label.Text = "Nombre";
+            // 
+            // municipio_label
+            // 
+            this.municipio_label.AutoSize = true;
+            this.municipio_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.municipio_label.ForeColor = System.Drawing.Color.White;
+            this.municipio_label.Location = new System.Drawing.Point(110, 313);
+            this.municipio_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.municipio_label.Name = "municipio_label";
+            this.municipio_label.Size = new System.Drawing.Size(152, 37);
+            this.municipio_label.TabIndex = 12;
+            this.municipio_label.Text = "Municipio";
+            // 
+            // rompiendoParadigmas_label
+            // 
+            this.rompiendoParadigmas_label.AutoSize = true;
+            this.rompiendoParadigmas_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rompiendoParadigmas_label.ForeColor = System.Drawing.SystemColors.Control;
+            this.rompiendoParadigmas_label.Location = new System.Drawing.Point(606, 573);
+            this.rompiendoParadigmas_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.rompiendoParadigmas_label.Name = "rompiendoParadigmas_label";
+            this.rompiendoParadigmas_label.Size = new System.Drawing.Size(183, 15);
+            this.rompiendoParadigmas_label.TabIndex = 13;
+            this.rompiendoParadigmas_label.Text = "ROMPIENDO PARADIGMAS";
+            // 
+            // guardar_btn
+            // 
+            this.guardar_btn.AutoSize = true;
+            this.guardar_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guardar_btn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.guardar_btn.Location = new System.Drawing.Point(342, 404);
+            this.guardar_btn.Name = "guardar_btn";
+            this.guardar_btn.Size = new System.Drawing.Size(123, 41);
+            this.guardar_btn.TabIndex = 14;
+            this.guardar_btn.Text = "Guardar";
+            this.guardar_btn.UseVisualStyleBackColor = true;
+            // 
+            // Crear_delegacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(175)))), ((int)(((byte)(216)))));
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.guardar_btn);
+            this.Controls.Add(this.rompiendoParadigmas_label);
+            this.Controls.Add(this.municipio_label);
+            this.Controls.Add(this.nombre_label);
+            this.Controls.Add(this.municipio_combo);
+            this.Controls.Add(this.atras_picture);
+            this.Controls.Add(this.close_picture);
+            this.Controls.Add(this.nombre_txt);
+            this.Controls.Add(this.delegacion_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Crear_delegacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.close_picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atras_picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nombre_txt;
+        private System.Windows.Forms.Label delegacion_label;
+        private System.Windows.Forms.PictureBox close_picture;
+        private System.Windows.Forms.PictureBox atras_picture;
+        private System.Windows.Forms.ComboBox municipio_combo;
+        private System.Windows.Forms.Label nombre_label;
+        private System.Windows.Forms.Label municipio_label;
+        private System.Windows.Forms.Label rompiendoParadigmas_label;
+        private System.Windows.Forms.Button guardar_btn;
+    }
+}
---
+++ CSEQ/CSEQ/Crear_marca.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace CSEQ
+{
+    partial class Crear_marca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Crear_marca));
+            this.guardar_btn = new System.Windows.Forms.Button();
+            this.rompiendoParadigmas_label = new System.Windows.Forms.Label();
+            this.nombre_label = new System.Windows.Forms.Label();
+            this.nombre_txt = new System.Windows.Forms.TextBox();
+            this.marca_label = new System.Windows.Forms.Label();
+            this.atras_picture = new System.Windows.Forms.PictureBox();
+            this.close_picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.atras_picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close_picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guardar_btn
+            // 
+            this.guardar_btn.AutoSize = true;
+            this.guardar_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guardar_btn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.guardar_btn.Location = new System.Drawing.Point(342, 405);
+            this.guardar_btn.Name = "guardar_btn";
+            this.guardar_btn.Size = new System.Drawing.Size(123, 41);
+            this.guardar_btn.TabIndex = 28;
+            this.guardar_btn.Text = "Guardar";
+            this.guardar_btn.UseVisualStyleBackColor = true;
+            // 
+            // rompiendoParadigmas_label
+            // 
+            this.rompiendoParadigmas_label.AutoSize = true;
+            this.rompiendoParadigmas_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.rompiendoParadigmas_label.ForeColor = System.Drawing.SystemColors.Control;
+            this.rompiendoParadigmas_label.Location = new System.Drawing.Point(606, 574);
+            this.rompiendoParadigmas_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.rompiendoParadigmas_label.Name = "rompiendoParadigmas_label";
+            this.rompiendoParadigmas_label.Size = new System.Drawing.Size(183, 15);
+            this.rompiendoParadigmas_label.TabIndex = 27;
+            this.rompiendoParadigmas_label.Text = "ROMPIENDO PARADIGMAS";
+            // 
+            // nombre_label
+            // 
+            this.nombre_label.AutoSize = true;
+            this.nombre_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_label.ForeColor = System.Drawing.Color.White;
+            this.nombre_label.Location = new System.Drawing.Point(144, 227);
+            this.nombre_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.nombre_label.Name = "nombre_label";
+            this.nombre_label.Size = new System.Drawing.Size(132, 37);
+            this.nombre_label.TabIndex = 26;
+            this.nombre_label.Text = "Nombre";
+            // 
+            // nombre_txt
+            // 
+            this.nombre_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre_txt.Location = new System.Drawing.Point(283, 227);
+            this.nombre_txt.MaximumSize = new System.Drawing.Size(400, 40);
+            this.nombre_txt.MinimumSize = new System.Drawing.Size(100, 4);
+            this.nombre_txt.Name = "nombre_txt";
+            this.nombre_txt.Size = new System.Drawing.Size(229, 38);
+            this.nombre_txt.TabIndex = 23;
+            // 
+            // marca_label
+            // 
+            this.marca_label.AutoSize = true;
+            this.marca_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.marca_label.ForeColor = System.Drawing.Color.White;
+            this.marca_label.Location = new System.Drawing.Point(346, 162);
+            this.marca_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.marca_label.Name = "marca_label";
+            this.marca_label.Size = new System.Drawing.Size(106, 37);
+            this.marca_label.TabIndex = 22;
+            this.marca_label.Text = "Marca";
+            // 
+            // atras_picture
+            // 
+            this.atras_picture.Image = ((System.Drawing.Image)(resources.GetObject("atras_picture.Image")));
+            this.atras_picture.Location = new System.Drawing.Point(12, 542);
+            this.atras_picture.Name = "atras_picture";
+            this.atras_picture.Size = new System.Drawing.Size(47, 47);
+            this.atras_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atras_picture.TabIndex = 25;
+            this.atras_picture.TabStop = false;
+            // 
+            // close_picture
+            // 
+            this.close_picture.Image = ((System.Drawing.Image)(resources.GetObject("close_picture.Image")));
+            this.close_picture.Location = new System.Drawing.Point(759, 12);
+            this.close_picture.Margin = new System.Windows.Forms.Padding(2);
+            this.close_picture.Name = "close_picture";
+            this.close_picture.Size = new System.Drawing.Size(30, 29);
+            this.close_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.close_picture.TabIndex = 24;
+            this.close_picture.TabStop = false;
+            this.close_picture.Click += new System.EventHandler(this.close_picture_Click);
+            // 
+            // Crear_marca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(175)))), ((int)(((byte)(216)))));
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.guardar_btn);
+            this.Controls.Add(this.rompiendoParadigmas_label);
+            this.Controls.Add(this.nombre_label);
+            this.Controls.Add(this.atras_picture);
+            this.Controls.Add(this.close_picture);
+            this.Controls.Add(this.nombre_txt);
+            this.Controls.Add(this.marca_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Crear_marca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crear_marca";
+            ((System.ComponentModel.ISupportInitialize)(this.atras_picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.close_picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button guardar_btn;
+        private System.Windows.Forms.Label rompiendoParadigmas_label;
+        private System.Windows.Forms.Label nombre_label;
+        private System.Windows.Forms.PictureBox atras_picture;
+        private System.Windows.Forms.PictureBox close_picture;
+        private System.Windows.Forms.TextBox nombre_txt;
+        private System.Windows.Forms.Label marca_label;
+    }
+}
