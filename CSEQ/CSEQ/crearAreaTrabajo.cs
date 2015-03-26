4acd18bcf18506feb5b18977e114ca62b89fff11
YuriUchiha <makubex_abj@hotmail.com>
2015-03-26 17:19:53 -0600

    Ventana de creaAreaTrabajo
    Después de 20 años xD. Perdón ^^U

---
+++ CSEQ/CSEQ/crearAreaTrabajo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSEQ
+{
+    public partial class crearAreaTrabajo : Form
+    {
+        public crearAreaTrabajo()
+        {
+            InitializeComponent();
+        }
+
+        private void close_picture_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void atras_picture_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Ventana.mostrarOculta(Ventana.Ventanas.ListaRegistros);
+        }
+    }
+}
