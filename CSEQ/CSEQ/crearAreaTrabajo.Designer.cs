4d5fbb71f78363a95947e08798a6bc6d815733a1
EduardoVaca <vacalalo@gmail.com>
2015-04-13 00:59:22 -0500

    Arregle tamano de areaTrabajo

--- CSEQ/CSEQ/crearAreaTrabajo.Designer.cs
+++ CSEQ/CSEQ/crearAreaTrabajo.Designer.cs
@@ -57,22 +57,22 @@
             this.labelAreaTrabajo.AutoSize = true;
             this.labelAreaTrabajo.Font = new System.Drawing.Font("Candara", 18F);
             this.labelAreaTrabajo.ForeColor = System.Drawing.Color.White;
-            this.labelAreaTrabajo.Location = new System.Drawing.Point(302, 33);
-            this.labelAreaTrabajo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelAreaTrabajo.Location = new System.Drawing.Point(604, 63);
+            this.labelAreaTrabajo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelAreaTrabajo.Name = "labelAreaTrabajo";
-            this.labelAreaTrabajo.Size = new System.Drawing.Size(169, 29);
+            this.labelAreaTrabajo.Size = new System.Drawing.Size(338, 59);
             this.labelAreaTrabajo.TabIndex = 23;
             this.labelAreaTrabajo.Text = "Área de trabajo";
             // 
             // nombreArea_txt
             // 
             this.nombreArea_txt.Font = new System.Drawing.Font("Candara", 14F);
-            this.nombreArea_txt.Location = new System.Drawing.Point(88, 471);
-            this.nombreArea_txt.Margin = new System.Windows.Forms.Padding(6);
-            this.nombreArea_txt.MaximumSize = new System.Drawing.Size(796, 40);
-            this.nombreArea_txt.MinimumSize = new System.Drawing.Size(196, 4);
+            this.nombreArea_txt.Location = new System.Drawing.Point(237, 390);
+            this.nombreArea_txt.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.nombreArea_txt.MaximumSize = new System.Drawing.Size(1588, 40);
+            this.nombreArea_txt.MinimumSize = new System.Drawing.Size(388, 4);
             this.nombreArea_txt.Name = "nombreArea_txt";
-            this.nombreArea_txt.Size = new System.Drawing.Size(306, 30);
+            this.nombreArea_txt.Size = new System.Drawing.Size(608, 40);
             this.nombreArea_txt.TabIndex = 24;
             // 
             // labelNombre
@@ -80,10 +80,10 @@
             this.labelNombre.AutoSize = true;
             this.labelNombre.Font = new System.Drawing.Font("Candara", 14F);
             this.labelNombre.ForeColor = System.Drawing.Color.White;
-            this.labelNombre.Location = new System.Drawing.Point(40, 202);
-            this.labelNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNombre.Location = new System.Drawing.Point(80, 388);
+            this.labelNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNombre.Name = "labelNombre";
-            this.labelNombre.Size = new System.Drawing.Size(83, 23);
+            this.labelNombre.Size = new System.Drawing.Size(162, 46);
             this.labelNombre.TabIndex = 25;
             this.labelNombre.Text = "Nombre:";
             // 
@@ -92,10 +92,10 @@
             this.guardar_btn.AutoSize = true;
             this.guardar_btn.Font = new System.Drawing.Font("Candara", 10F);
             this.guardar_btn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.guardar_btn.Location = new System.Drawing.Point(44, 762);
-            this.guardar_btn.Margin = new System.Windows.Forms.Padding(6);
+            this.guardar_btn.Location = new System.Drawing.Point(130, 643);
+            this.guardar_btn.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.guardar_btn.Name = "guardar_btn";
-            this.guardar_btn.Size = new System.Drawing.Size(80, 32);
+            this.guardar_btn.Size = new System.Drawing.Size(160, 62);
             this.guardar_btn.TabIndex = 29;
             this.guardar_btn.Text = "Guardar";
             this.guardar_btn.UseVisualStyleBackColor = true;
@@ -104,10 +104,10 @@
             // close_picture
             // 
             this.close_picture.Image = ((System.Drawing.Image)(resources.GetObject("close_picture.Image")));
-            this.close_picture.Location = new System.Drawing.Point(1523, 11);
-            this.close_picture.Margin = new System.Windows.Forms.Padding(2);
+            this.close_picture.Location = new System.Drawing.Point(1544, 13);
+            this.close_picture.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.close_picture.Name = "close_picture";
-            this.close_picture.Size = new System.Drawing.Size(30, 29);
+            this.close_picture.Size = new System.Drawing.Size(60, 56);
             this.close_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.close_picture.TabIndex = 30;
             this.close_picture.TabStop = false;
@@ -116,10 +116,10 @@
             // atras_picture
             // 
             this.atras_picture.Image = ((System.Drawing.Image)(resources.GetObject("atras_picture.Image")));
-            this.atras_picture.Location = new System.Drawing.Point(12, 937);
-            this.atras_picture.Margin = new System.Windows.Forms.Padding(6);
+            this.atras_picture.Location = new System.Drawing.Point(8, 1025);
+            this.atras_picture.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.atras_picture.Name = "atras_picture";
-            this.atras_picture.Size = new System.Drawing.Size(30, 29);
+            this.atras_picture.Size = new System.Drawing.Size(60, 56);
             this.atras_picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.atras_picture.TabIndex = 31;
             this.atras_picture.TabStop = false;
@@ -130,10 +130,10 @@
             this.rompiendoParadigmas_label.AutoSize = true;
             this.rompiendoParadigmas_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.rompiendoParadigmas_label.ForeColor = System.Drawing.SystemColors.Control;
-            this.rompiendoParadigmas_label.Location = new System.Drawing.Point(613, 500);
-            this.rompiendoParadigmas_label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.rompiendoParadigmas_label.Location = new System.Drawing.Point(1245, 1025);
+            this.rompiendoParadigmas_label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.rompiendoParadigmas_label.Name = "rompiendoParadigmas_label";
-            this.rompiendoParadigmas_label.Size = new System.Drawing.Size(183, 15);
+            this.rompiendoParadigmas_label.Size = new System.Drawing.Size(342, 29);
             this.rompiendoParadigmas_label.TabIndex = 32;
             this.rompiendoParadigmas_label.Text = "ROMPIENDO PARADIGMAS";
             // 
@@ -141,9 +141,10 @@
             // 
             this.eliminar_btn.Enabled = false;
             this.eliminar_btn.Font = new System.Drawing.Font("Candara", 10F);
-            this.eliminar_btn.Location = new System.Drawing.Point(612, 762);
+            this.eliminar_btn.Location = new System.Drawing.Point(644, 643);
+            this.eliminar_btn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.eliminar_btn.Name = "eliminar_btn";
-            this.eliminar_btn.Size = new System.Drawing.Size(80, 32);
+            this.eliminar_btn.Size = new System.Drawing.Size(160, 62);
             this.eliminar_btn.TabIndex = 70;
             this.eliminar_btn.Text = "Eliminar";
             this.eliminar_btn.UseVisualStyleBackColor = true;
@@ -153,10 +154,10 @@
             // 
             this.modificar_btn.Enabled = false;
             this.modificar_btn.Font = new System.Drawing.Font("Candara", 10F);
-            this.modificar_btn.Location = new System.Drawing.Point(328, 762);
-            this.modificar_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.modificar_btn.Location = new System.Drawing.Point(394, 643);
+            this.modificar_btn.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
             this.modificar_btn.Name = "modificar_btn";
-            this.modificar_btn.Size = new System.Drawing.Size(80, 32);
+            this.modificar_btn.Size = new System.Drawing.Size(160, 62);
             this.modificar_btn.TabIndex = 69;
             this.modificar_btn.Text = "Modificar";
             this.modificar_btn.UseVisualStyleBackColor = true;
@@ -168,11 +169,11 @@
             this.groupBox1.Controls.Add(this.busqueda_txt);
             this.groupBox1.Font = new System.Drawing.Font("Candara", 9.75F);
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(832, 181);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Location = new System.Drawing.Point(867, 145);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
-            this.groupBox1.Size = new System.Drawing.Size(708, 723);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.groupBox1.Size = new System.Drawing.Size(726, 705);
             this.groupBox1.TabIndex = 71;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Búsqueda de registros";
@@ -191,7 +192,6 @@
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.busqueda_grid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.busqueda_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.busqueda_grid.Margin = new System.Windows.Forms.Padding(6);
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(175)))), ((int)(((byte)(216)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Candara", 9.75F);
@@ -201,7 +201,8 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.busqueda_grid.DefaultCellStyle = dataGridViewCellStyle2;
             this.busqueda_grid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.busqueda_grid.Location = new System.Drawing.Point(6, 108);
+            this.busqueda_grid.Location = new System.Drawing.Point(12, 208);
+            this.busqueda_grid.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.busqueda_grid.Name = "busqueda_grid";
             dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(175)))), ((int)(((byte)(216)))));
@@ -211,17 +212,17 @@
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.busqueda_grid.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            this.busqueda_grid.Size = new System.Drawing.Size(342, 262);
+            this.busqueda_grid.Size = new System.Drawing.Size(684, 504);
             this.busqueda_grid.TabIndex = 56;
             this.busqueda_grid.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.busqueda_grid_RowEnter);
             // 
             // Buscar
             // 
             this.Buscar.Image = ((System.Drawing.Image)(resources.GetObject("Buscar.Image")));
-            this.Buscar.Location = new System.Drawing.Point(610, 62);
-            this.Buscar.Margin = new System.Windows.Forms.Padding(6);
+            this.Buscar.Location = new System.Drawing.Point(636, 131);
+            this.Buscar.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.Buscar.Name = "Buscar";
-            this.Buscar.Size = new System.Drawing.Size(39, 36);
+            this.Buscar.Size = new System.Drawing.Size(78, 69);
             this.Buscar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Buscar.TabIndex = 55;
             this.Buscar.TabStop = false;
@@ -230,22 +231,22 @@
             // busqueda_txt
             // 
             this.busqueda_txt.Font = new System.Drawing.Font("Candara", 12F);
-            this.busqueda_txt.Location = new System.Drawing.Point(12, 75);
-            this.busqueda_txt.Margin = new System.Windows.Forms.Padding(6);
-            this.busqueda_txt.MaximumSize = new System.Drawing.Size(796, 40);
-            this.busqueda_txt.MinimumSize = new System.Drawing.Size(196, 4);
+            this.busqueda_txt.Location = new System.Drawing.Point(24, 144);
+            this.busqueda_txt.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.busqueda_txt.MaximumSize = new System.Drawing.Size(1588, 40);
+            this.busqueda_txt.MinimumSize = new System.Drawing.Size(388, 4);
             this.busqueda_txt.Name = "busqueda_txt";
-            this.busqueda_txt.Size = new System.Drawing.Size(296, 27);
+            this.busqueda_txt.Size = new System.Drawing.Size(588, 47);
             this.busqueda_txt.TabIndex = 8;
             // 
             // crearAreaTrabajo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(175)))), ((int)(((byte)(216)))));
-            this.ClientSize = new System.Drawing.Size(1594, 1012);
+            this.ClientSize = new System.Drawing.Size(1600, 1092);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.eliminar_btn);
             this.Controls.Add(this.modificar_btn);
@@ -257,7 +258,7 @@
             this.Controls.Add(this.nombreArea_txt);
             this.Controls.Add(this.labelAreaTrabajo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
             this.Name = "crearAreaTrabajo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "crearAreaTrabajo";
