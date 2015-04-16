269a426ec1d6226a9f2db496e4581790fefc3899
EduardoVaca <vacalalo@gmail.com>
2015-04-16 11:51:07 -0500

    arregle conexion con crystal reports

--- CSEQ/CSEQ/Reporte.cs
+++ CSEQ/CSEQ/Reporte.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CrystalDecisions.Shared;
+using CrystalDecisions.CrystalReports.Engine;
 
 namespace CSEQ
 {
